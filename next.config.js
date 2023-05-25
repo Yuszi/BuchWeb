4b2994690e2e2d4d07a2b9867c81e5c6f4e940ff
Yusuf Uzun <uzyu1011@h-ka.de>
2023-05-25 13:49:15 +0200

    next configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {swcMinify: true, pageExtensions:['tsx', 'ts']}
+const nextConfig = {
+    reactStrictMode: true,
+}
 
 module.exports = nextConfig
