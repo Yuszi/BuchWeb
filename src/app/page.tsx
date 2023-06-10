0eb2a4d611c8f6660b1c4ab2306a26a9506f1772
Chiara Scheurer <scch1091@h-ka.de>
2023-06-11 00:24:07 +0200

    bugfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,24 @@
+import '../app/global.scss'
 import SearchBook from './SearchBook';
 import Head from 'next/head';
 import Script from 'next/script';
 import './page.module.css'; 
 import Image from 'next/image'
- 
+import profilePic from './hintergrund.png'
+import { useEffect } from 'react';
 
-export default function Page() {
+
+export default function Page(...args: []) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-          crossOrigin="anonymous"
-        />
       </Head>
 
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossOrigin="anonymous"
-      />
+        crossOrigin="anonymous" />
       <SearchBook />
     </>
   );
