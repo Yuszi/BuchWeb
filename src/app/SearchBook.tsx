77e3bded8b604db4ef7d27072dba5a1390ce8c51
Dmytro Stadnytskyi <dima092339@gmail.com>
2023-06-12 09:40:58 +0200

    Rework filters and add a reset filter button

--- src/app/SearchBook.tsx
+++ src/app/SearchBook.tsx
@@ -4,16 +4,37 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 import styles from './page.module.css';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 
 export default function SearchBook() {
   const [pathId, SetPathId] = useState('');
   const [path, SetPath] = useState('');
   const [placeholderText, SetPlaceholderText] = useState('');
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const defaultSearchBarPlaceholderText = 'Press a Button for search option';
+  const defaultSearchButtonText = 'Filter';
 
   useEffect(() => {
     console.log(pathId);
-    SetPlaceholderText('Press a Button for search option');
+    SetPlaceholderText(defaultSearchBarPlaceholderText);
   }, [pathId]);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const onFilterChange = (selectedPath: string, selectedPlaceholder: string) => {
+    SetPath(selectedPath);
+    SetPlaceholderText(selectedPlaceholder);
+    setDropdownOpen(false);
+  }
+
+  const resetFilter = () => {
+    SetPath('');
+    SetPathId('');
+    SetPlaceholderText(defaultSearchBarPlaceholderText)
+  }
 
   return (
     <>
@@ -29,29 +50,43 @@ export default function SearchBook() {
           style={{ width: '250%' }}
         />
       </div>
-      <div>
-        <div className="btn-group" role="group" aria-label="Basic example">
+      <div className='d-flex'>
+        <div className="dropdown">
           <button
+            className="btn btn-secondary dropdown-toggle"
             type="button"
-            className="btn btn-secondary"
-            onClick={() => {
-              SetPath('titel');
-              SetPlaceholderText('Enter Titel');
-            }}
-            >
-            Titel
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-            onClick={() => {
-              SetPath('isbn');
-              SetPlaceholderText('Enter ISBN');
-            }}
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded={dropdownOpen}
+            onClick={toggleDropdown}
           >
-            ISBN
+            {defaultSearchButtonText}: {path}
           </button>
+          <div
+            className={`dropdown-menu ${dropdownOpen ? 'show' : ''}`}
+            aria-labelledby="dropdownMenuButton">
+            <button
+              className="dropdown-item"
+              onClick={() => onFilterChange('titel', 'Enter Titel')}
+              >
+              Titel
+            </button>
+            <button
+              className="dropdown-item"
+              onClick={() => onFilterChange('isbn', 'Enter ISBN')}
+            >
+              ISBN
+            </button>
+          </div>
         </div>
+        <button 
+        type='button' 
+        className='btn btn-secondary m-lg-2'
+        onClick={resetFilter}
+        >
+          Reset filter
+        </button>
       </div>
       <div>
         <br />
